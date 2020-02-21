60402fd7a97037afaba92a3845aea16567f141a0
mamchurrr <s.mamchurrr@gmail.com>
2020-02-21 12:05:50 +0200

    fix(BeachEventSideButtons): logic buttons

--- components/pages/beach-event/BeachEventSideButtons.vue
+++ components/pages/beach-event/BeachEventSideButtons.vue
@@ -1,28 +1,57 @@
 <template>
-	<div class="beach-event__side-buttons">
-		<button class="bg-orange" v-if="share">
-			<img src="~/static/pics/global/svg/share.svg">
-		</button>
-		<button v-if="mail">
-			<img src="~/static/pics/global/svg/mail.svg">
-		</button>
-		<button v-if="telegram">
-			<img src="~/static/pics/global/svg/telegram-side.svg">
-		</button>
-		<button :class="{ 'bg-blue': crossBlue, 'bg-orange': crossOrange }">
-			<img src="~/static/pics/global/svg/cross.svg">
-		</button>
-		<button v-if="ymaps">
-			<img src="~/static/pics/global/svg/ymaps.svg">
-		</button>
-		<button v-if="yandex">
-			<img src="~/static/pics/global/svg/yandex.svg">
-		</button>
-	</div>
+    <div class="beach-event__side-buttons">
+        <button v-if="showShare">
+            <img src="~/static/pics/global/svg/mail.svg">
+        </button>
+        <button v-if="showShare">
+            <img src="~/static/pics/global/svg/telegram-side.svg">
+        </button>
+        <button
+            class="bg-orange"
+            v-if="showShare"
+            @click="showShare = !showShare"
+        >
+            <img src="~/static/pics/global/svg/cross.svg">
+        </button>
+        <button
+            class="bg-orange"
+            v-if="!showShare"
+            @click="showShare = !showShare"
+        >
+            <img src="~/static/pics/global/svg/share.svg">
+        </button>
+
+        <button
+            class="bg-blue"
+            v-if="!showPave"
+            @click="showPave = !showPave"
+        >
+            <img src="~/static/pics/global/svg/pave_way.svg">
+        </button>
+        <button
+            class="bg-blue"
+            v-if="showPave"
+            @click="showPave = !showPave"
+        >
+            <img src="~/static/pics/global/svg/cross.svg">
+        </button>
+        <button v-if="ymaps && showPave">
+            <img src="~/static/pics/global/svg/ymaps.svg">
+        </button>
+        <button v-if="yandex && showPave">
+            <img src="~/static/pics/global/svg/yandex.svg">
+        </button>
+    </div>
 </template>
 
 <script>
-	export default {
-		props: ['share', 'crossBlue', 'crossOrange', 'ymaps', 'yandex', 'mail', 'telegram']
-	}
+export default {
+    props: ['share', 'crossBlue', 'crossOrange', 'ymaps', 'yandex', 'mail', 'telegram'],
+    data () {
+        return {
+            showShare: false,
+            showPave: false
+        };
+    },
+}
 </script>
