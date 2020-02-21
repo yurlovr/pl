f386c506006525ec533fa7374b521d98659e085f
mamchurrr <s.mamchurrr@gmail.com>
2020-02-21 13:04:13 +0200

    fix(BeachBarsNRestos): adaptiv

--- components/pages/beach-event/BeachEventSections.vue
+++ components/pages/beach-event/BeachEventSections.vue
@@ -1,47 +1,57 @@
 <template>
-	<div class="beach-page-sections slider-weather__months">
-		<div class="custom-container">
-			<div v-swiper:mySwiper="swiperOption">
-				<div class="swiper-wrapper">
-					<div class="swiper-slide" v-for="(section, i) in sections" @click="activeSection = i" :class="{ active : i == activeSection }">
-						<nuxt-link :to="{path: '#id-'+i, hash: '#id-'+i}" class="slider-weather__month beach-page-sections__section" :class="{ active : i == activeSection }">
-							<span>{{ section }}</span>
-						</nuxt-link>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="beach-page-sections slider-weather__months">
+        <div class="custom-container">
+            <div v-swiper:mySwiper="swiperOption">
+                <div class="swiper-wrapper">
+                    <div
+                        class="swiper-slide"
+                        v-for="(section, i) in sections"
+                        @click="activeSection = i"
+                        :key="i+0.2"
+                        :class="{ active : i == activeSection }"
+                    >
+                        <nuxt-link
+                            :to="{path: '#id-'+i, hash: '#id-'+i}"
+                            class="slider-weather__month beach-page-sections__section"
+                            :class="{ active : i == activeSection }"
+                        >
+                            <span>{{ section }}</span>
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-	import Vue from 'vue';
+import Vue from 'vue';
 
-	export default {
-		props: ['sections'],
+export default {
+    props: ['sections'],
 
-		beforeMount () {
-			if (process.browser) {
-				require('swiper/dist/css/swiper.css');
-				const VueAwesomeSwiper = require('vue-awesome-swiper/dist/ssr');
-				Vue.use(VueAwesomeSwiper);
-			}
-		},
+    beforeMount () {
+        if (process.browser) {
+            require('swiper/dist/css/swiper.css');
+            const VueAwesomeSwiper = require('vue-awesome-swiper/dist/ssr');
+            Vue.use(VueAwesomeSwiper);
+        }
+    },
 
-		data() {
-			return {
-				activeSection: 0,
-				swiperOption: {
-					freeMode: true,
-					spaceBetween: 0,
-					slidesPerView: 'auto',
-					init: false
-				}
-			}
-		},
+    data () {
+        return {
+            activeSection: 0,
+            swiperOption: {
+                freeMode: true,
+                spaceBetween: 0,
+                slidesPerView: 'auto',
+                init: false
+            }
+        };
+    },
 
-		mounted() {
-			this.mySwiper.init(this.swiperOption);
-		}
-	}
+    mounted () {
+        this.mySwiper.init(this.swiperOption);
+    }
+}
 </script>
--- components/pages/beach/BeachBarsNRestos.vue
+++ components/pages/beach/BeachBarsNRestos.vue
@@ -1,68 +1,108 @@
 <template>
-	<section class="beach-page__barsNrestos">
-		<h2 class="two-part-layout__card__title beach-page__barsNrestos__title">Бары и рестораны</h2>
-		<div v-swiper:mySwiper="swiperOption">
-			<div class="swiper-wrapper">
-				<div class="swiper-slide" v-for="(item, i) in data" :key="i">
-					<div class="beach-page__barsNrestos__subtitle-area">
-						<h3 class="beach-page__barsNrestos__subtitle">{{ item.title }}</h3>
-						<div class="beach-page__barsNrestos__arrow-controls">
-							<button @click="mySwiper.slidePrev()">
-								<img src="~/static/pics/beach/arrow_left_orange.svg" alt="Налево">
-							</button>
-							<span class="beach-page__barsNrestos__arrow-controls__text"><span class="orange">{{ i+1 }}</span>/{{ data.length }}</span>
-							<button @click="mySwiper.slideNext()">
-								<img src="~/static/pics/beach/arrow_right_orange.svg" alt="Направо">
-							</button>
-						</div>
-					</div>
-					<p class="beach-page__barsNrestos__description">
-						{{ item.description }}
-					</p>
-					<a href="#" class="beach-page__barsNrestos__online-service">
-						<img src="~/static/pics/beach/online_service.svg">
-						<span>Онлайн <br>обслуживание</span>
-					</a>
-					<SliderBeachBarsNRestos :data="item.pics" />
-				</div>
-			</div>
-		</div>
-	</section>
+    <section class="beach-page__barsNrestos">
+        <h2 class="two-part-layout__card__title beach-page__barsNrestos__title">Бары и рестораны</h2>
+        <div v-swiper:mySwiper="swiperOption">
+            <div class="swiper-wrapper">
+                <div
+                    class="swiper-slide"
+                    v-for="(item, i) in data"
+                    :key="i"
+                >
+                    <div>
+                        <a
+                            href="#"
+                            class="beach-page__barsNrestos__online-service swiper-bar-display_none"
+                        >
+                            <img src="~/static/pics/beach/online_service.svg">
+                            <span>Онлайн <br>обслуживание</span>
+                        </a>
+                    </div>
+                    <div class="beach-page__barsNrestos__subtitle-area">
+                        <h3 class="beach-page__barsNrestos__subtitle">{{ item.title }}</h3>
+
+                        <div class="beach-page__barsNrestos__arrow-controls swiper-bar-display">
+                            <button @click="mySwiper.slidePrev()">
+                                <img
+                                    src="~/static/pics/beach/arrow_left_orange.svg"
+                                    alt="Налево"
+                                >
+                            </button>
+                            <span class="beach-page__barsNrestos__arrow-controls__text"><span class="orange">{{ i+1 }}</span>/{{ data.length }}</span>
+                            <button @click="mySwiper.slideNext()">
+                                <img
+                                    src="~/static/pics/beach/arrow_right_orange.svg"
+                                    alt="Направо"
+                                >
+                            </button>
+                        </div>
+                    </div>
+                    <p class="beach-page__barsNrestos__description">
+                        {{ item.description }}
+                    </p>
+                    <a
+                        href="#"
+                        class="beach-page__barsNrestos__online-service swiper-bar-display"
+                    >
+                        <img src="~/static/pics/beach/online_service.svg">
+                        <span>Онлайн <br>обслуживание</span>
+                    </a>
+                    <SliderBeachBarsNRestos :data="item.pics" />
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
-	import Vue from 'vue';
-	import SliderBeachBarsNRestos from '~/components/pages/beach/SliderBeachBarsNRestos';
+import Vue from 'vue';
+import SliderBeachBarsNRestos from '~/components/pages/beach/SliderBeachBarsNRestos';
 
-	export default {
-		props: ['data'],
+export default {
+    props: ['data'],
 
-		beforeMount () {
-			if (process.browser) {
-				require('swiper/dist/css/swiper.css');
-				const VueAwesomeSwiper = require('vue-awesome-swiper/dist/ssr');
-				Vue.use(VueAwesomeSwiper);
-			}
-		},
+    beforeMount () {
+        if (process.browser) {
+            require('swiper/dist/css/swiper.css');
+            const VueAwesomeSwiper = require('vue-awesome-swiper/dist/ssr');
+            Vue.use(VueAwesomeSwiper);
+        }
+    },
 
-		components: {
-			SliderBeachBarsNRestos
-		},
+    components: {
+        SliderBeachBarsNRestos
+    },
 
-		data() {
-			return {
-				swiperOption: {
-					spaceBetween: 70,
-					simulateTouch: false,
-					allowTouchMove: false,
-					slidesPerView: 1,
-					init: false
-				}
-			}
-		},
+    data () {
+        return {
+            swiperOption: {
+                spaceBetween: 70,
+                simulateTouch: false,
+                allowTouchMove: false,
+                slidesPerView: 1,
+                init: false
+            }
+        };
+    },
 
-		mounted() {
-			this.mySwiper.init(this.swiperOption);
-		}
-	}
+    mounted () {
+        this.mySwiper.init(this.swiperOption);
+    }
+}
 </script>
+
+<style lang="scss" scoped>
+.swiper-bar-display {
+    display: block;
+    &_none {
+        display: none;
+    }
+}
+@media (max-width: 768px) {
+    .swiper-bar-display {
+        display: none;
+        &_none {
+            display: flex;
+        }
+    }
+}
+</style>
