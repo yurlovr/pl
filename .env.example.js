0322ad3df7e370f03ad5b89d010d0e94c04a97d1
vlad <v389116470@gmail.com>
2020-04-11 00:17:48 +0300

    .env.example update

--- .env.example.js
+++ .env.example.js
@@ -1,1 +1,2 @@
-export const url_api = 'https://crimea.air-dev.agency/api/app'
+export const url_api = 'https://crimea.air-dev.agency/api/app/'
+export const pic_url = 'https://crimea.air-dev.agency'
