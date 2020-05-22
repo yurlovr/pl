75e6ef4f88ec7b635217476ee864fa4d0e25a1f0
vlad <v389116470@gmail.com>
2020-05-22 22:57:13 +0300

    svg heart full rework

--- components/global/AddToFavorites.vue
+++ components/global/AddToFavorites.vue
@@ -1,7 +1,13 @@
 <template>
 	<button class="heart" @click.prevent="updateHeart()" @mouseenter="updateHeartHover()" @mouseleave="updateHeartHover()" v-if="data.showFavorite != undefined">
-		<img  src="~/static/pics/global/svg/heart_unclicked.svg" alt="В избранное" v-show="!favorite && !favoriteHover">
-		<img  src="~/static/pics/global/svg/heart_clicked.svg" alt="В избранное" v-show="favorite || favoriteHover && !favorite">
+<!--		<img  src="~/static/pics/global/svg/heart_unclicked.svg" alt="В избранное" v-show="!favorite && !favoriteHover">
+		<img  src="~/static/pics/global/svg/heart_clicked.svg" alt="В избранное" v-show="favorite || favoriteHover && !favorite">-->
+    <svg class="svg-heart" width="12" height="11" viewBox="0 0 23 22" xmlns="http://www.w3.org/2000/svg">
+      <path
+        :class="[{lovely: favorite || favoriteHover && !favorite}, {unknown: !favorite && !favoriteHover}]"
+        d="M6.4114 18.2772C8.93676 20.5476 11.2238 21.8947 11.3196 21.9511C11.3758 21.9837 11.4378 22 11.5002 22C11.5626 22 11.6246 21.9837 11.6804 21.9511C11.7766 21.8951 14.0629 20.5476 16.5879 18.2775C18.933 16.1684 21.9103 12.8427 22.7763 8.97267C23.7425 4.65581 21.4196 1.53737 18.8521 0.467244C16.1418 -0.662585 13.3093 0.300012 11.5002 2.91786C9.69077 0.299641 6.85822 -0.664069 4.14717 0.467244C1.58011 1.53775 -0.742509 4.65581 0.223677 8.97267C1.08867 12.8423 4.06629 16.1684 6.4114 18.2772Z"
+        fill="#FEE6C0"/>
+    </svg>
 	</button>
 </template>
 
