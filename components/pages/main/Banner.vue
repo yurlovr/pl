8de292cc56512942ec13557ac2f37059192543f9
vlad <v389116470@gmail.com>
2020-05-29 19:58:34 +0300

    v-html

--- components/pages/main/Banner.vue
+++ components/pages/main/Banner.vue
@@ -6,8 +6,7 @@
 			</div>
 			<div class="banner__card__info-area">
 				<h4 class="banner__card__info-area__title" v-html="getTitle"></h4>
-				<p class="banner__card__info-area__text">
-					{{ data.description }}
+				<p class="banner__card__info-area__text" v-html="data.description">
 				</p>
 				<a :href="data.link" @click="$bus.goTo(data.link, $router)" class="banner__card__info-area__button">
 					<span>{{ data.buttonText }}</span>
