6f2af8611daf1e902edcfaf41b291d367d04d2df
july_july <yaleksasina@gmail.com>
2020-07-10 19:54:04 +0300

    #375

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="content">
+    <script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://vk.com/js/api/openapi.js?168",t.onload=function()
+
+    {VK.Retargeting.Init("VK-RTRG-505686-cbKtO"),VK.Retargeting.Hit()}
+      ,document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-505686-cbKtO" style="position:fixed; left:-999px;" alt=""/></noscript>
+
     <PageTransitioner />
     <Preloader />
     <Header />
