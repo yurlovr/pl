683a35972d0900c090f3c07535f22848179989ec
Olga2017 <olga.s@cronix.ms>
2020-07-14 14:30:12 +0300

    # [fix]: pages: hide empty block

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -30,7 +30,7 @@
 			<BeachEventVisitorPics id="visitor-pics" :data="eventData.visitorPics" :type="'event'" :typeId="eventData.mainData.eventId" />
 		</div>
 		<BeachSliderArea id="other-events" class="beach-event__similar-beaches" :data="eventData.otherEvents" v-if="eventData.otherEvents.beachNumber > 0" />
-    <div class="main-page__white-wrapper" v-if="eventData.another_places">
+    <div class="main-page__white-wrapper" v-if="eventData.another_places.length > 0">
       <BeachSliderArea :data="eventData.another_places" class="main-page__family-rest" outlink="https://nash.travel/hotel" />
     </div>
 	</div>
