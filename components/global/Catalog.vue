84e51454030b296bd7eac26d37cabbcb60aa509b
vlad <v389116470@gmail.com>
2020-08-06 13:53:27 +0300

    catalog fix

--- components/global/Catalog.vue
+++ components/global/Catalog.vue
@@ -1,21 +1,21 @@
 <template>
-	<div class="catalog-page custom-page" v-if="data">
-		<div class="catalog-page__title-area custom-container">
-			<h3 class="custom-page__title catalog-page__title">{{ data.title }}</h3>
-			<div class="search-page__title-area__buttons" v-if="data && type == 'beach'">
-				<button class="search-page__title-area__button" :class="{ active: !showCardsOrMap }" @click="showCardsOrMap = false">
-					<img  src="~/static/pics/search/cards_orange.svg" alt="Вид: Карточки" v-show="!showCardsOrMap">
-					<img  src="~/static/pics/search/cards_gray.svg" alt="Вид: Карточки" v-show="showCardsOrMap">
-				</button>
-				<button class="search-page__title-area__button" :class="{ active: showCardsOrMap }" @click="showMap()">
-					<img  src="~/static/pics/search/map_orange.svg" alt="Вид: Карта" v-show="showCardsOrMap">
-					<img  src="~/static/pics/search/map_gray.svg" alt="Вид: Карта" v-show="!showCardsOrMap">
-				</button>
-			</div>
-		</div>
-		<CardGrid :perPage="20" :data="data.grid" v-if="data.grid" v-show="!showCardsOrMap" />
-		<SearchMapArea :data="data.grid" v-show="showCardsOrMap" v-if="data.grid && data.grid.length > 0 && type == 'beach'" />
-	</div>
+  <div class="catalog-page custom-page" v-if="data">
+    <div class="catalog-page__title-area custom-container">
+      <h3 class="custom-page__title catalog-page__title">{{ data.title }}</h3>
+      <div class="search-page__title-area__buttons" v-if="data && type == 'beach'">
+        <button class="search-page__title-area__button" :class="{ active: !showCardsOrMap }" @click="showCardsOrMap = false">
+          <img  src="~/static/pics/search/cards_orange.svg" alt="Вид: Карточки" v-show="!showCardsOrMap">
+          <img  src="~/static/pics/search/cards_gray.svg" alt="Вид: Карточки" v-show="showCardsOrMap">
+        </button>
+        <button class="search-page__title-area__button" :class="{ active: showCardsOrMap }" @click="showMap()">
+          <img  src="~/static/pics/search/map_orange.svg" alt="Вид: Карта" v-show="showCardsOrMap">
+          <img  src="~/static/pics/search/map_gray.svg" alt="Вид: Карта" v-show="!showCardsOrMap">
+        </button>
+      </div>
+    </div>
+    <CardGrid :perPage="20" :data="data.grid" v-if="data.grid" v-show="!showCardsOrMap" />
+    <SearchMapArea :data="data.grid" v-show="showCardsOrMap" v-if="data.grid && data.grid.length > 0 && type == 'beach'" />
+  </div>
 </template>
 
 <script>
@@ -25,27 +25,22 @@ import SearchMapArea from '~/components/pages/search/SearchMapArea';
 import { mapGetters, mapMutations } from 'vuex';
 
 export default {
-	props: ['type'],
+  props: ['type'],
 
-	components: {
-		CardGrid,
-		SearchMapArea
-	},
+  components: {
+    CardGrid,
+    SearchMapArea
+  },
 
-	computed: {
-		...mapGetters('catalog', ['data'])
-	},
+  computed: {
+    ...mapGetters('catalog', ['data'])
+  },
 
-	data() {
-		return {
-			showCardsOrMap: false, // cards: false, map: true
-			mapShownForTheFirstTime: false,
-		}
-	},
-  async asyncData( {$axios, route}){
-    const {data} = await $axios.$get('seo/meta?url=' + route.fullPath)
+  data() {
     return {
-      meta: data
+      showCardsOrMap: false, // cards: false, map: true
+      mapShownForTheFirstTime: false,
+      meta: {}
     }
   },
   head() {
@@ -63,16 +58,24 @@ export default {
     }
   },
 
-	methods: {
-		...mapMutations('catalog', ['setType', 'setQuery']),
+  async created() {
+    await this.$axios.$get('seo/meta?url=' + this.$route.fullPath).then(res => {
+      this.meta = res.data
+    })
+    this.setType(this.type);
+    this.setQuery(this.$router.currentRoute.query);
+  },
 
-		showMap() {
-			this.showCardsOrMap = true;
-			if (!this.mapShownForTheFirstTime) {
-				this.mapShownForTheFirstTime = true;
-				this.$bus.$emit('updateScrollbar');
-			}
-		}
-	}
+  methods: {
+    ...mapMutations('catalog', ['setType', 'setQuery']),
+
+    showMap() {
+      this.showCardsOrMap = true;
+      if (!this.mapShownForTheFirstTime) {
+        this.mapShownForTheFirstTime = true;
+        this.$bus.$emit('updateScrollbar');
+      }
+    }
+  }
 }
 </script>
