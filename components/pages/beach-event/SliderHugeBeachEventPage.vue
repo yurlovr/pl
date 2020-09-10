1e081799a65e3457c77e1dfa9bec4d8d2aaed971
julia-alex <yaleksasina@gmail.com>
2020-09-10 16:33:02 +0300

    #607

--- components/pages/beach-event/SliderHugeBeachEventPage.vue
+++ components/pages/beach-event/SliderHugeBeachEventPage.vue
@@ -1,7 +1,7 @@
 <style scoped>
-  .z-index-preview {
-    z-index: 2;
-  }
+.z-index-preview {
+  z-index: 2;
+}
 </style>
 <template>
   <div class="slider-beach-event">
@@ -104,7 +104,8 @@
         </div>
         <div class="pagination-wrapper" v-if="data.medium_pics.length > 1">
           <div class="custom-pagination">
-            <button @click="mySwiper.slideTo(i)" class="custom-pagination-bullet" v-for="(b,i) in data.medium_pics.length"
+            <button @click="mySwiper.slideTo(i)" class="custom-pagination-bullet"
+                    v-for="(b,i) in data.medium_pics.length"
                     :class="{ 'custom-pagination-bullet-active' : i == activeIndex }"></button>
           </div>
         </div>
@@ -149,7 +150,9 @@
       <div class="slider-beach-event__modal__inner">
         <div class="slider-beach-event__modal__title-area">
           <h2 class="slider-beach-event__modal__title" v-html="data.title"></h2>
-          <span class="slider-beach-event__modal__count"><span class="orange">{{ activeIndex+1 }}</span>/{{ data.pics.length }}</span>
+          <span class="slider-beach-event__modal__count"><span class="orange">{{
+              activeIndex + 1
+            }}</span>/{{ data.pics.length }}</span>
         </div>
         <div class="slider-beach-event__modal__sides big-big-big">
           <!--          <CoolLightBox-->
@@ -163,7 +166,8 @@
               <div class="swiper-wrapper">
                 <div class="swiper-slide" v-for="(pic, i) in data.pics" :key="i">
                   <div class="position-relative">
-                        <span v-if="!pic.includes('youtube')" class="zoom-img" @click="$refs.beachZoomModal.showZoomModal(pic)" v-show="zoom_plus_show">
+                        <span v-if="!pic.includes('youtube')" class="zoom-img"
+                              @click="$refs.beachZoomModal.showZoomModal(pic)" v-show="zoom_plus_show">
                              <img src="~/static/pics/search/loop_plus.svg">
                         </span>
                     <div v-if="!pic.includes('youtube')">
@@ -171,10 +175,10 @@
                         <img src="~/static/pics/beach/size.png">
                         <div class="stamp-text-size">
                           <span>ОРИГИНАЛ</span>
-                          <b>{{ (data.sizes[i]/1024/1024).toFixed(1)}} МБ</b>
+                          <b>{{ (data.sizes[i] / 1024 / 1024).toFixed(1) }} МБ</b>
                         </div>
                       </div>
-                          <img v-lazy-load :data-src="pic">
+                      <img v-lazy-load :data-src="pic">
                     </div>
                     <div v-else class="w-100 h-100 y-block">
                       <!--                    active in modal-->
@@ -194,6 +198,12 @@
                     @click="() => { mySwiperModal.slideNext(); }">
               <img src="~/static/pics/global/svg/slider_arrow_right.svg" alt="Направо">
             </button>
+            <div class="pagination-wrapper" v-if="data.medium_pics.length > 1">
+              <div class="custom-pagination">
+                <button @click="mySwiper.slideTo(i)" class="custom-pagination-bullet" v-for="(b,i) in data.medium_pics.length"
+                        :class="{ 'custom-pagination-bullet-active' : i == activeIndex }"></button>
+              </div>
+            </div>
           </div>
           <div class="slider-beach-event__modal__right big-modal-right">
             <div v-swiper:mySwiperModalSmall="swiperModalSmallOption">
@@ -239,176 +249,176 @@
 </template>
 
 <script>
-  import Vue from 'vue';
-  import VideoYoutube from '../beach/video-tube'
-  import {getIdFromUrl} from 'vue-youtube';
-  import CoolLightBox from 'vue-cool-lightbox'
-  import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
-  import VueZoomer from 'vue-zoomer'
-  import BeachZoomImage from "./BeachZoomImage";
+import Vue from 'vue';
+import VideoYoutube from '../beach/video-tube'
+import {getIdFromUrl} from 'vue-youtube';
+import CoolLightBox from 'vue-cool-lightbox'
+import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css'
+import VueZoomer from 'vue-zoomer'
+import BeachZoomImage from "./BeachZoomImage";
 
 
-  export default {
-    props: ['data'],
-    components: {
-      BeachZoomImage,
-      VideoYoutube,
-      CoolLightBox,
+export default {
+  props: ['data'],
+  components: {
+    BeachZoomImage,
+    VideoYoutube,
+    CoolLightBox,
+  },
+
+  beforeMount() {
+    if (process.browser) {
+      require('swiper/dist/css/swiper.css');
+      const VueAwesomeSwiper = require('vue-awesome-swiper/dist/ssr');
+      Vue.use(VueAwesomeSwiper);
+      Vue.use(VueZoomer);
+    }
+  },
+
+  computed: {
+    getRightPicsHeight() {
+      if (Math.min(this.data.pics.length - 1, 6) % 2 == 0)
+        return `${(100 / (Math.min(this.data.pics.length - 1, 6) / 2)).toFixed(2)}%`;
+      else return `${(100 / (Math.min(this.data.pics.length, 6) / 2)).toFixed(2)}%`;
     },
-
-    beforeMount() {
-      if (process.browser) {
-        require('swiper/dist/css/swiper.css');
-        const VueAwesomeSwiper = require('vue-awesome-swiper/dist/ssr');
-        Vue.use(VueAwesomeSwiper);
-        Vue.use(VueZoomer);
-      }
+    player() {
+      return this.$refs['youtube'].player
     },
+    isEvent() {
+      return this.$route.path.includes('event')
+    }
+  },
 
-    computed: {
-      getRightPicsHeight() {
-        if (Math.min(this.data.pics.length - 1, 6) % 2 == 0)
-          return `${(100 / (Math.min(this.data.pics.length - 1, 6) / 2)).toFixed(2)}%`;
-        else return `${(100 / (Math.min(this.data.pics.length, 6) / 2)).toFixed(2)}%`;
-      },
-      player() {
-        return this.$refs['youtube'].player
-      },
-      isEvent() {
-        return this.$route.path.includes('event')
-      }
-    },
-
-    data() {
-      return {
-        zoom_plus_show: true,
-        interval_show: 0,
-        interval_plus: null,
-        index: null,
-        mobile: 1024,
-        swiperOption: {
-          spaceBetween: 20,
-          slidesPerView: 1,
-          observer: true,
-          observeParents: true,
-          init: false,
-          breakpoints: {
-            550: {
-              spaceBetween: 10
-            }
-          },
-          keyboard: {
-            enabled: true,
-            onlyInViewport: false,
+  data() {
+    return {
+      zoom_plus_show: true,
+      interval_show: 0,
+      interval_plus: null,
+      index: null,
+      mobile: 1024,
+      swiperOption: {
+        spaceBetween: 20,
+        slidesPerView: 1,
+        observer: true,
+        observeParents: true,
+        init: false,
+        breakpoints: {
+          550: {
+            spaceBetween: 10
           }
         },
-        showLeft: false,
-        showRight: true,
-        activeIndex: 0,
-        modalOpen: false,
-        swiperModalSmallOption: {
-          slidesPerView: 'auto',
-          spaceBetween: 15,
-          direction: 'vertical',
-          mousewheel: true,
-          init: false,
-          breakpoints: {
-            600: {
-              direction: 'horizontal',
-              spaceBetween: 10
-            }
-          }
+        keyboard: {
+          enabled: true,
+          onlyInViewport: false,
         }
-      }
-    },
-    created() {
-      if (process.browser) {
-        this.mobile = window.innerWidth
-        window.onresize = () => {
-          this.mobile = window.innerWidth
-        }
-      }
-    },
-
-    mounted() {
-      this.mySwiper.on('slideChange', () => {
-        if (!this.modalOpen)
-          this.mySwiperModal.slideTo(this.mySwiper.activeIndex);
-        this.mySwiperModalSmall.slideTo(this.mySwiper.activeIndex);
-        this.updateArrows();
-        this.activeIndex = this.mySwiper.activeIndex;
-      });
-
-      this.mySwiperModal.on('slideChange', () => {
-        if (this.modalOpen) {
-          this.interval_show = 0;
-          clearInterval(this.interval_plus);
-          this.mySwiper.slideTo(this.mySwiperModal.activeIndex);
-          this.zoom_plus_show = true;
-          this.zoomController();
-        }
-      });
-
-      this.mySwiper.init(this.swiperOption);
-      this.mySwiperModal.init(this.swiperOption);
-      this.mySwiperModalSmall.init(this.swiperOption);
-
-      // this.updateArrows();
-    },
-
-    methods: {
-      zoomController(){
-        console.warn('zoom controller')
-        this.interval_plus = setInterval(() => {
-          this.interval_show++;
-          if (this.interval_show >= 2) {
-            this.zoom_plus_show = false;
-            clearInterval(this.interval_plus)
-          }
-        }, 1000)
       },
-      bigModalOpen(){
-        this.modalOpen = !this.modalOpen;
-        this.zoomController();
-      },
-      showPhotoSwipe(index) {
-        this.isOpen = true
-        this.$set(this.optionsGallery, 'index', index)
-      },
-      hidePhotoSwipe() {
-        this.isOpen = false
-      },
-      openCamera(url) {
-        window.open(url, '_blank');
-      },
-      transformUrl(s) {
-        return getIdFromUrl(s)
-      },
-      async playVideo(e) {
-        await this.player.playVideo()
-      },
-      updateArrows() {
-        this.showLeft = !this.mySwiper.isBeginning;
-        this.showRight = !this.mySwiper.isEnd;
-      },
-
-      openModal(i, video = false, pos = 'main-') {
-        this.mySwiper.slideTo(i);
-        /*if (video && i <= 4) {
-          setTimeout(() => {
-            document.getElementById('v1').click();
-          }, 200)
-        }*/
-        if (i > 5 || video) {
-          this.modalOpen = true;
-          if (video) {
-            setTimeout(() => {
-              console.log(pos + i, 'pos + i')
-              document.getElementById(pos + i).click();
-            }, 200)
+      showLeft: false,
+      showRight: true,
+      activeIndex: 0,
+      modalOpen: false,
+      swiperModalSmallOption: {
+        slidesPerView: 'auto',
+        spaceBetween: 15,
+        direction: 'vertical',
+        mousewheel: true,
+        init: false,
+        breakpoints: {
+          600: {
+            direction: 'horizontal',
+            spaceBetween: 10
           }
         }
       }
     }
+  },
+  created() {
+    if (process.browser) {
+      this.mobile = window.innerWidth
+      window.onresize = () => {
+        this.mobile = window.innerWidth
+      }
+    }
+  },
+
+  mounted() {
+    this.mySwiper.on('slideChange', () => {
+      if (!this.modalOpen)
+        this.mySwiperModal.slideTo(this.mySwiper.activeIndex);
+      this.mySwiperModalSmall.slideTo(this.mySwiper.activeIndex);
+      this.updateArrows();
+      this.activeIndex = this.mySwiper.activeIndex;
+    });
+
+    this.mySwiperModal.on('slideChange', () => {
+      if (this.modalOpen) {
+        this.interval_show = 0;
+        clearInterval(this.interval_plus);
+        this.mySwiper.slideTo(this.mySwiperModal.activeIndex);
+        this.zoom_plus_show = true;
+        this.zoomController();
+      }
+    });
+
+    this.mySwiper.init(this.swiperOption);
+    this.mySwiperModal.init(this.swiperOption);
+    this.mySwiperModalSmall.init(this.swiperOption);
+
+    // this.updateArrows();
+  },
+
+  methods: {
+    zoomController() {
+      console.warn('zoom controller')
+      this.interval_plus = setInterval(() => {
+        this.interval_show++;
+        if (this.interval_show >= 2) {
+          this.zoom_plus_show = false;
+          clearInterval(this.interval_plus)
+        }
+      }, 1000)
+    },
+    bigModalOpen() {
+      this.modalOpen = !this.modalOpen;
+      this.zoomController();
+    },
+    showPhotoSwipe(index) {
+      this.isOpen = true
+      this.$set(this.optionsGallery, 'index', index)
+    },
+    hidePhotoSwipe() {
+      this.isOpen = false
+    },
+    openCamera(url) {
+      window.open(url, '_blank');
+    },
+    transformUrl(s) {
+      return getIdFromUrl(s)
+    },
+    async playVideo(e) {
+      await this.player.playVideo()
+    },
+    updateArrows() {
+      this.showLeft = !this.mySwiper.isBeginning;
+      this.showRight = !this.mySwiper.isEnd;
+    },
+
+    openModal(i, video = false, pos = 'main-') {
+      this.mySwiper.slideTo(i);
+      /*if (video && i <= 4) {
+        setTimeout(() => {
+          document.getElementById('v1').click();
+        }, 200)
+      }*/
+      if (i > 5 || video) {
+        this.modalOpen = true;
+        if (video) {
+          setTimeout(() => {
+            console.log(pos + i, 'pos + i')
+            document.getElementById(pos + i).click();
+          }, 200)
+        }
+      }
+    }
   }
+}
 </script>
