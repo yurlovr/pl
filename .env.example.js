30660d258790a2816408fcc566b56d9c508648e9
Vasiliy <nahy@ya.ru>
2020-09-29 12:36:02 +0300

    Add env vars

--- .env.example.js
+++ .env.example.js
@@ -1,1 +1,3 @@
 export const url_api = process.env.URL_API || 'https://backuat.plyazhi.ru/api/app/'
+export const prom_host = process.env.PROM_HOST || '0.0.0.0'
+export const prom_port = process.env.PROM_PORT || 8091
