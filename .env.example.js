fb3085d914ed1484d393cf206aec7c99de6714c2
simon <babysharny@gmail.com>
2020-09-30 17:29:43 +0300

    BEACH-732

--- .env.example.js
+++ .env.example.js
@@ -1,1 +1,2 @@
+export const sentry_dsn = 'https://6ee3cdf313e7467f99bfcd466c6f430a@sentry.nashtr.support/3'
 export const url_api = process.env.URL_API || 'https://backuat.plyazhi.ru/api/app/'
--- plugins/sentry.js
+++ plugins/sentry.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import * as Sentry from '@sentry/browser';
 import { Vue as VueIntegration } from '@sentry/integrations';
 import {sentry_dsn} from '../.env.js'
-// Sentry.init({
-//   dsn: sentry_dsn,
-//   integrations: [new VueIntegration({Vue, attachProps: true})],
-// });
+Sentry.init({
+  dsn: sentry_dsn,
+  integrations: [new VueIntegration({
+    Vue,
+    attachProps: true,
+    tracing: true,
+    tracingOptions: {
+      trackComponents: true,
+    },
+  })],
+});
