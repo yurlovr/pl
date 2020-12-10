7f20fa3cc02547900fe7181940d3e464cd6a6db2
vlad <v389116470@gmail.com>
2020-12-10 19:33:41 +0300

    #402 yandex zoom

--- components/pages/beach-event/BeachEventPtMap.vue
+++ components/pages/beach-event/BeachEventPtMap.vue
@@ -52,7 +52,7 @@
               });
               this.map.behaviors.disable('scrollZoom');
               this.$el.querySelector('.map__zoom-plus').addEventListener('click', () => {
-                if (this.zoom < 18) {
+                if (this.zoom < 24) {
                   this.zoom++;
                   this.map.setZoom(this.zoom);
                 }
